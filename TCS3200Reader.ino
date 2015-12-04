d87bf8a7a3de831ea9bb777f76231f948df57b93
KeiranCantilina <keirancantilina@gmail.com>
2015-12-04 16:21:46 -0500

    Update TCS3200Reader.ino
    One that works, hopefully. ><

--- TCS3200Reader.ino
+++ TCS3200Reader.ino
@@ -1,76 +1,66 @@
-#include <TimerOne.h>
+// Code for running color sensor
+// Remember: Frequency-In ("OUT") must be on Digital Pin 5 (or whatever is mapped to the 16 Bit Hardware Counter1)
 
-#define OUT  2
-#define S2   10
-#define S3   11
-#define S0   8
-#define S1   9
+#include <FreqCounter.h> 				// Start frequency counting library
+
+int S0_pin = 8;							// Initiate settings pins, variables, etc.
+int S1_pin = 9;
+int S2_pin = 10;
+int S3_pin = 11;
+
+int LED_pin = A0;
+
+long int red;
+long int green;
+long int blue;
+long int white;
 
 void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(9600);
-  pinMode(OUT, INPUT);
-  pinMode(S2, OUTPUT);
-  pinMode(S3, OUTPUT);
-  pinMode(S0, OUTPUT);
-  pinMode(S1, OUTPUT);
+  Serial.begin(9600);                   // Connect to the serial port
+  Serial.println("Color Sensor");		// If nothing else works, at least this should
+  digitalWrite(S0, HIGH);				// Set attenuation (Default is HIGH LOW, which is attenuation to 20%)
+  digitalWrite(S1, LOW);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  digitalWrite(S0, LOW);    // 2% of Output Frequency Range
-  digitalWrite(S1, HIGH);
-  int R=0;
-  int G=0;
-  int B=0;
-  int W=0;
-  int phi=analogRead(A0);
 
-  //Red
-  digitalWrite(S2, LOW);    
-  digitalWrite(S3, LOW);
-  Timer1.setPeriod(10000);
-  while(digitalRead(OUT)==1){
-    R++;
-    break;
-  }
-
-  //Green
-  digitalWrite(S2, HIGH);    
-  digitalWrite(S3, HIGH);
-  Timer1.setPeriod(10000);
-  while(digitalRead(OUT)==1){
-    G++;
-    break;
-  }
-
-  //Blue
-  digitalWrite(S2, LOW);    
-  digitalWrite(S3, HIGH);
-  Timer1.setPeriod(10000);
-  while(digitalRead(OUT)==1){
-    B++;
-    break;
-  }
-  
-  //White
-  digitalWrite(S2, HIGH);    
-  digitalWrite(S3, LOW);
-  Timer1.setPeriod(10000);
-  while(digitalRead(OUT)==1){
-    W++;
-    break;
-  }
-  
-  Serial.print(millis());
-  Serial.print(",");
-  Serial.print(phi);
-  Serial.print(",");
-  Serial.print(W);
-  Serial.print(",");
-  Serial.print(R);
-  Serial.print(",");
-  Serial.print(G);
-  Serial.print(",");
-  Serial.println(B);
+	FreqCounter::f_comp= 8;             // Set compensation to 12
+	
+	digitalWrite(S2, HIGH);				// Set config pins
+	digitalWrite(S3, LOW);
+	FreqCounter::start(100);            // Start counting with gatetime of 100ms
+	while (FreqCounter::f_ready == 0)   // Wait until counter ready
+	white=FreqCounter::f_freq;			// Read result
+	
+	digitalWrite(S2, LOW);
+	digitalWrite(S3, LOW);
+	FreqCounter::start(100);            
+	while (FreqCounter::f_ready == 0)   
+	red=FreqCounter::f_freq;            
+ 
+	digitalWrite(S2, HIGH);
+	digitalWrite(S3, HIGH);
+	FreqCounter::start(100);            
+	while (FreqCounter::f_ready == 0)   
+	green=FreqCounter::f_freq;
+ 
+	digitalWrite(S2, LOW);
+	digitalWrite(S3, HIGH);
+	FreqCounter::start(100);            
+	while (FreqCounter::f_ready == 0)   
+	blue=FreqCounter::f_freq;
+ 
+	LED = analogRead(LED_pin);			// Read ghetto photodiode voltage
+	
+	Serial.print(millis());				// ALL of the Serial.print
+	Serial.print(",");
+	Serial.print(LED);
+	Serial.print(",");
+	Serial.print(white);
+	Serial.print(",");
+	Serial.print(red);
+	Serial.print(",");
+	Serial.print(green);
+	Serial.print(",");
+	Serial.println(blue);
 }
